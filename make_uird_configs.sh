9d673bad90a54ac3fc25bcf0e81b9eb96239ca79
Anton Goroshkin <neobht@gmail.com>
2015-01-10 16:01:49 +0300

    rename make_configs.sh -> make_uird_configs.sh

--- make_uird_configs.sh
+++ make_uird_configs.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-#rm -rf /usr/lib/dracut/modules.d/97uird /usr/lib/dracut/modules.d/98magos-soft /usr/lib/dracut/modules.d/90ntfs
-#cp -pRf modules.d/* /usr/lib/dracut/modules.d
-
 #dracut -N  -f -m "base busybox uird magos-soft network ntfs url-lib ifcfg"  \
 dracut -N  -f -m ""  \
         --confdir "dracut.conf.d" \
