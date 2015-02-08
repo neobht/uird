421601e48eed5b84c29f5d08d08b2e8b47ec4dd5
Anton Goroshkin <neobht@gmail.com>
2015-02-08 14:40:32 +0300

    add drivers & search mountdir changed for support ROSA,Fedora iso

--- make_uird_magos.sh
+++ make_uird_magos.sh
@@ -4,14 +4,17 @@ cp -pRf modules.d/* /usr/lib/dracut/modules.d
 echo $(pwd)
 dracut  -N  -f -m "base uird uird-network ntfs kernel-modules"  \
 	-d "loop cryptoloop aes-generic aes-i586 pata_acpi ata_generic ahci xhci-hcd \
-	    af_packet 3c59x acenic de4x5 e1000 e1000e e100 epic100 hp100 ne2k-pci pcnet32 8139too 8139cp tulip via-rhine r8169 atl1e yellowfin tg3 dl2k ns83820 atl1 b44 bnx2 skge sky2 tulip depca 3c501 3c503 3c505 3c507 3c509 3c515 ac3200 at1700 cosa cs89x0 de600 de620 e2100 eepro eexpress eth16i ewrk3 forcedeth hostess_sv11 hp-plus hp ni52 ni65 sb1000 sealevel smc-ultra sis900 smc9194  wd \
 	    usb-storage uhci-hcd hid usbhid ehci-hcd ohci-hcd ehci-pci ehci-platform hid-generic \
-	    sr_mod ata sd_mod scsi_mod ide \
-	    nls_cp866 nls_utf8 nfs nfs_acl jbd jbd2 lockd" \
+	    sr_mod sd_mod scsi_mod \ 
+	    nls_cp866 nls_utf8 nfs nfs_acl jbd jbd2 lockd \
+	    af_packet \
+	    =ide =ata =ethernet =usb/storage =usb/host" \
         --filesystems "aufs squashfs vfat msdos iso9660 isofs xfs ext3 ext4 fuse nfs cifs udf" \
         --confdir "dracut.conf.d" \
         -i initrd / \
         --kernel-cmdline "uird.from=/MagOS,/MagOS-Data uird.ro=*.xzm,*.rom,*.rom.enc,*.pfs,*.sfs uird.rw=*.rwm,*.rwm.enc uird.cp=*.xzm.cp uird.load=* uird.noload=/optional/,/machines/,/homes/,/cache/ uird.machines=/MagOS-Data/machines uird.config=MagOS.ini" \
         -c dracut.conf -v -M uird.magos.cpio.xz $(uname -r) >dracut.log 2>&1
+
+#	    af_packet 3c59x acenic de4x5 e1000 e1000e e100 epic100 hp100 ne2k-pci pcnet32 8139too 8139cp tulip via-rhine r8169 atl1e yellowfin tg3 dl2k ns83820 atl1 b44 bnx2 skge sky2 tulip depca 3c501 3c503 3c505 3c507 3c509 3c515 ac3200 at1700 cosa cs89x0 de600 de620 e2100 eepro eexpress eth16i ewrk3 forcedeth hostess_sv11 hp-plus hp ni52 ni65 sb1000 sealevel smc-ultra sis900 smc9194  wd \
 
 #        -c dracut.conf -v -M uird.magos.cpio.xz 3.14.17-pf4 >dracut.log 2>&1
