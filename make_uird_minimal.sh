adf088f9f7a6e128eb349287339120850f931a40
Anton Goroshkin <neobht@gmail.com>
2015-03-15 12:28:00 +0300

    some fixes uird.minimal

--- make_uird_minimal.sh
+++ make_uird_minimal.sh
@@ -3,6 +3,7 @@ rm -rf /usr/lib/dracut/modules.d/97uird
 cp -pRf modules.d/* /usr/lib/dracut/modules.d
 
 dracut -N  -f -m "base uird"  \
+        -i initrd / \
         --confdir "dracut.conf.d" \
         --kernel-cmdline "uird.from=/MagOS,/MagOS-Data uird.ro=*.xzm,*.rom,*.rom.enc,*.pfs,*.sfs uird.rw=*.rwm,*.rwm.enc uird.load=* uird.noload=/optional/,/machines/,/homes/,/cache/ uird.machines=/MagOS-Data/machines" \
         -c dracut.conf -v -M uird.minimal.cpio.xz $(uname -r) >dracut.log 2>&1
