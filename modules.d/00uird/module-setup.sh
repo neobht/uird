3aa2ee4999582895f1e1eea6382da4d766c25841
betcher <betkher.al@gmail.com>
2016-03-20 20:07:27 +0300

    added uird.scan

--- modules.d/00uird/module-setup.sh
+++ modules.d/00uird/module-setup.sh
@@ -21,6 +21,7 @@ install() {
     inst "$moddir/livekit/livekitlib" "/livekitlib"
     inst "$moddir/livekit/uird-init" "/uird-init"
     inst "$moddir/livekit/liblinuxlive" "/liblinuxlive"
+    inst "$moddir/livekit/uird.scan" "/uird.scan"
     inst "$moddir/livekit/i18n/ru.mo" "/usr/share/locale/ru/LC_MESSAGES/uird.mo"
 
     #binaries
