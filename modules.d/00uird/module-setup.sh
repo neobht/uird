d9d93ffa3043c0087ad5557b8be4ba60f25bbaa7
betcher <betkher.al@gmail.com>
2017-11-24 21:53:34 +0300

    testing zramfs

--- modules.d/00uird/module-setup.sh
+++ modules.d/00uird/module-setup.sh
@@ -16,7 +16,7 @@ installkernel() {
 }
 
 
-bins="locale dialog gettext loadkeys resume rsync fsck fsck.ext2 fsck.ext3 fsck.ext4 fsck.exfat fsck.vfat fsck.xfs fsck.btrfs btrfsck ntfsfix btfs aria2c"
+bins="locale dialog gettext loadkeys resume rsync fsck fsck.ext2 fsck.ext3 fsck.ext4 fsck.exfat fsck.vfat fsck.xfs fsck.btrfs btrfsck ntfsfix  aria2c mkfs.ext4"
 
 
 install() {
