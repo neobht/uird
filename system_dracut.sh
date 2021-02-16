4dc3f613a45a4cf770c0d0be94a61b1e2d2d207b
betcher <betkher.al@mail.ru>
2021-02-16 16:05:28 +0700

    system_dracut.s fixed

--- system_dracut.sh
+++ system_dracut.sh
@@ -9,10 +9,11 @@ mkdir -p "${WORKDIR}/dracut/dracut.conf.d" "${WORKDIR}/dracut/modules.d"
 for a in init logger functions ; do
     ln -s /usr/lib/dracut/dracut-${a}.sh ${WORKDIR}/dracut/
 done
-ln -s "$(which dracut)" ${WORKDIR}/dracut/dracut.sh
-ln -s "$(which dracut-install)" ${WORKDIR}/dracut/dracut-install
 
-ln -s ${WORKDIR}modules.d/* "${WORKDIR}/dracut/modules.d/"
+ln -s "$(which dracut-install)" ${WORKDIR}/dracut/dracut-install
+ln -s "$(which dracut)" ${WORKDIR}/dracut/dracut.sh
+
+ln -s ${WORKDIR}/modules.d/* "${WORKDIR}/dracut/modules.d/"
 ln -s /usr/lib/dracut/modules.d/* "${WORKDIR}/dracut/modules.d/"
 
 
