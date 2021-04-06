3b6a1523f50709c371681e57d2496f4c46b2e2b2
betcher <betkher.al@mail.ru>
2021-04-06 12:41:11 +0700

    system_dracut.sh fixed

--- system_dracut.sh
+++ system_dracut.sh
@@ -1,9 +1,12 @@
 #!/bin/bash 
-WORKDIR=$(realpath ./)
+WORKDIR="$(dirname $0)"
 [ $1 ] && WORKDIR=$(realpath "$1")
 if [ -d ${WORKDIR}/dracut ] ; then
-    echo   "${WORKDIR}/dracut already exists"
-    exit 1
+    echo "${WORKDIR}/dracut already exists"
+    echo "Enter (a/A) to abort, or another key to continue"
+    read qqq
+    [ "$qqq" == 'a' -o "$qqq" == "A" ] && exit 1
+    rm -rf ${WORKDIR}/dracut
 fi
 mkdir -p "${WORKDIR}/dracut/dracut.conf.d" "${WORKDIR}/dracut/modules.d"
 for a in init logger functions ; do
@@ -16,10 +19,3 @@ ln -s "$(which dracut)" ${WORKDIR}/dracut/dracut.sh
 ln -s ${WORKDIR}/modules.d/* "${WORKDIR}/dracut/modules.d/"
 ln -s /usr/lib/dracut/modules.d/* "${WORKDIR}/dracut/modules.d/"
 
-
-
-
-
-
-
- 
