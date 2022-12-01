59c7b13fa4fd382e6c7ad38713be85a544582ec0
betcher <a.betkher@rosalinux.ru>
2022-12-01 15:44:09 +0300

    fix for make_dracut.sh

--- make_dracut.sh
+++ make_dracut.sh
@@ -3,10 +3,10 @@
 git submodule init
 git submodule update
 cd dracut
-patch -N -p1 < ../0001-fix-for-shutdown-using-plymouth.patch || exit %{LINENO}
+patch -N -p1 < ../0001-fix-for-shutdown-using-plymouth.patch
 #busybox setfont не умеет без путей
 #TODO шрифт должен устанавливаться с локалью 
-sed -i 's:^DEFAULT_FONT=.*:DEFAULT_FONT=/usr/lib/consolefonts/ru/UniCyr_8x14.psf' /modules.d/10i18n/console_init.sh || exit %{LINENO}
+sed -i 's:^DEFAULT_FONT=.*:DEFAULT_FONT=/usr/lib/consolefonts/ru/UniCyr_8x14.psf:' modules.d/10i18n/console_init.sh || exit %{LINENO}
 make clean
 ./configure --disable-documentation
 make -j $(( $(nproc) + 1 ))
