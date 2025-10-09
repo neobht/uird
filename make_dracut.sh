77c1f7b93874069085b58f618878eeec62454694
betcher <a.betkher@rosa.ru>
2025-10-09 23:48:25 +0700

    Hide udevadm warning

--- make_dracut.sh
+++ make_dracut.sh
@@ -4,6 +4,7 @@ git submodule init
 git submodule update
 pushd dracut-ng
 patch -N -p1 < ../0001-fix-for-shutdown-using-plymouth.patch
+patch -N -p1 < ../0001-Hide-udevadm-warning-message.patch
 #busybox setfont не умеет без путей
 #TODO шрифт должен устанавливаться с локалью 
 sed -i 's:^DEFAULT_FONT=.*:DEFAULT_FONT=/usr/lib/consolefonts/ru/UniCyr_8x14.psf:' modules.d/10i18n/console_init.sh || exit %{LINENO}
