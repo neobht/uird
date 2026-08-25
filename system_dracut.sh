3532d88c353105c3a05f599e7b7f1f986f6923f6
betcher <a.betkher@rosa.ru>
2026-08-25 19:49:54 +0700

    Fix for system_dracut.sh

--- system_dracut.sh
+++ system_dracut.sh
@@ -1,22 +1,45 @@
 #!/bin/bash 
 # Author: Alexander Betkher <http://magos-linux.ru>
-WORKDIR="$(dirname $0)"
-[ $1 ] && WORKDIR=$(realpath "$1")
-if [ -d ${WORKDIR}/dracut ] ; then
-    echo "${WORKDIR}/dracut already exists"
-    echo "Enter (a/A) to abort, or another key to continue"
-    read qqq
-    [ "$qqq" == 'a' -o "$qqq" == "A" ] && exit 1
-    rm -rf ${WORKDIR}/dracut
+
+# Если передан аргумент, используем его как имя папки, иначе "dracut"
+if [ -n "$1" ]; then
+    DIR_NAME="$1"
+    FORCE=true
+else
+    DIR_NAME="dracut"
+    FORCE=false
 fi
-mkdir -p "${WORKDIR}/dracut/dracut.conf.d" "${WORKDIR}/dracut/modules.d"
+
+# Определяем рабочий каталог как текущий каталог + имя папки
+WORKDIR="$(pwd)/${DIR_NAME}"
+
+# Проверяем, существует ли уже папка
+if [ -d "${WORKDIR}" ] ; then
+    if [ "$FORCE" = true ]; then
+        # Если $1 указан - удаляем без вопросов
+        echo "Removing existing ${WORKDIR}..."
+        rm -rf "${WORKDIR}"
+    else
+        # Если $1 не указан - спрашиваем
+        echo "${WORKDIR} already exists"
+        echo "Enter (a/A) to abort, or another key to continue"
+        read qqq
+        [ "$qqq" == 'a' -o "$qqq" == "A" ] && exit 1
+        rm -rf "${WORKDIR}"
+    fi
+fi
+
+# Создаем структуру каталогов
+mkdir -p "${WORKDIR}/dracut.conf.d" "${WORKDIR}/modules.d"
+
+# Создаем ссылки на файлы dracut
 for a in init logger functions ; do
-    ln -s /usr/lib/dracut/dracut-${a}.sh ${WORKDIR}/dracut/
+    ln -s /usr/lib/dracut/dracut-${a}.sh ${WORKDIR}/
 done
 
-ln -s "$(which dracut-install)" ${WORKDIR}/dracut/dracut-install
-ln -s "$(which dracut)" ${WORKDIR}/dracut/dracut.sh
+ln -s "$(which dracut-install)" ${WORKDIR}/dracut-install
+ln -s "$(which dracut)" ${WORKDIR}/dracut.sh
 
-ln -s ${WORKDIR}/modules.d/* "${WORKDIR}/dracut/modules.d/"
-ln -s /usr/lib/dracut/modules.d/* "${WORKDIR}/dracut/modules.d/"
-
+# Создаем ссылки на модули
+ln -s ${WORKDIR}/../modules.d/* "${WORKDIR}/modules.d/" 2>/dev/null || true
+ln -s /usr/lib/dracut/modules.d/* "${WORKDIR}/modules.d/"
